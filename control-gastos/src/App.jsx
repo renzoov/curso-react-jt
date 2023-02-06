865ea23f2e09852d6ad128fd0e0426869a492eed
Renzo Osorio <renzo.osorio1909@gmail.com>
2023-02-06 09:30:45 -0500

    primer commit

---
+++ control-gastos/src/App.jsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+
+import Header from "./components/Header";
+import Modal from "./components/Modal";
+import { generarId } from "./helpers";
+import IconoNuevogasto from "./img/nuevo-gasto.svg";
+
+function App() {
+  const [presupuesto, setPresupuesto] = useState(0);
+  const [isValidPresupuesto, setIsValidPresupuesto] = useState(false);
+  const [modal, setModal] = useState(false);
+  const [animarModal, setAnimarModal] = useState(false);
+  const [gastos, setGastos] = useState([]);
+
+  const handleNuevoGasto = () => {
+    setModal(true);
+
+    setTimeout(() => {
+      setAnimarModal(true);
+    }, 500);
+  };
+
+  const guardarGasto = (gasto) => {
+    setGastos([...gastos, gasto]);
+  };
+
+  return (
+    <div>
+      <Header
+        presupuesto={presupuesto}
+        setPresupuesto={setPresupuesto}
+        isValidPresupuesto={isValidPresupuesto}
+        setIsValidPresupuesto={setIsValidPresupuesto}
+      />
+
+      {isValidPresupuesto && (
+        <div className="nuevo-gasto">
+          <img
+            src={IconoNuevogasto}
+            alt="icono nuevo gasto"
+            onClick={handleNuevoGasto}
+          />
+        </div>
+      )}
+
+      {modal && (
+        <Modal
+          setModal={setModal}
+          animarModal={animarModal}
+          setAnimarModal={setAnimarModal}
+          guardarGasto={guardarGasto}
+        />
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ control-gastos/src/components/Header.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+import NuevoPresupuesto from "./NuevoPresupuesto";
+import ControlPresupuesto from "./ControlPresupuesto";
+
+const Header = ({
+  presupuesto,
+  setPresupuesto,
+  isValidPresupuesto,
+  setIsValidPresupuesto,
+}) => {
+  return (
+    <header>
+      <h1>Planificador de gastos</h1>
+
+      {isValidPresupuesto ? (
+        <ControlPresupuesto presupuesto={presupuesto} />
+      ) : (
+        <NuevoPresupuesto
+          presupuesto={presupuesto}
+          setPresupuesto={setPresupuesto}
+          setIsValidPresupuesto={setIsValidPresupuesto}
+        />
+      )}
+    </header>
+  );
+};
+
+export default Header;
