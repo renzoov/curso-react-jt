77e7a6d198eabae4d035384d4cff6a4203bc607e
Renzo Osorio <renzo.osorio1909@gmail.com>
2023-02-09 22:51:41 -0500

    cotizador criptomonedas terminado

---
+++ criptos-react/src/data/monedas.js
@@ -0,0 +1,9 @@
+const monedas = [
+  { id: "USD", nombre: "Dolar de Estados Unidos" },
+  { id: "MXN", nombre: "Peso Mexicano" },
+  { id: "EUR", nombre: "Euro" },
+  { id: "GBP", nombre: "Libra Esperlina" },
+  { id: "PEN", nombre: "Nuevo Sol" },
+];
+
+export { monedas };
